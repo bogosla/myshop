96c54e9c24a9269d536d30bda7b6cb352ccfeea2
bogosla <jdestine68@gmail.com>
2020-07-25 14:21:53 -0400

    cart app

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-from decimal import decimal
+from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
@@ -9,8 +9,7 @@ class Cart(object):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            self.session[settings.CART_SESSION_ID] = {}
-
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
@@ -24,11 +23,11 @@ class Cart(object):
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
-
         self.save()
 
     def save(self):
         self.session.modified = True
+
     def remove(self, product):
         product_id = str(product.id)
         if product_id in self.cart:
@@ -41,8 +40,9 @@ class Cart(object):
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
+
         for item in cart.values():
-            item['price'] = decimal(item['price'])
+            item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
 
             yield item
--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 
 ]
